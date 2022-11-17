a33fabf2f7473caecce7047ce0e4e639df03231f
MACOIL <workedcoding@gmail.com>
2022-11-17 22:45:42 +0700

    Update Website

--- script.js
+++ script.js
@@ -1,49 +1,26 @@
 // Alternate Fuction
 const navPort = document.querySelector(".navPortfolio");
-const navPorts = Array.from(navPort.children);
-const myPort = document.querySelector(".containerPort");
-const myPorts = Array.from(myPort.children);
+const myPort = document.querySelectorAll(".gallery");
 
-const updateNav = (currentPort, targetNav) => {
-    currentPort.classList.remove('active');
-    targetNav.classList.add('active');
-}
+window.onload = () => {
+    navPort.onclick = (selectedItem) => {
+        if (selectedItem.target.classList.contains("folio")) {
+            navPort.querySelector(".active").classList.remove('active');
+            selectedItem.target.classList.add('active');
 
-function changePage () {
-    for(let i = 0; i < myPorts.length; i++) {
-        myPorts[i].classList.remove('active');
+            let navName = selectedItem.target.getAttribute("data-name");
+
+            myPort.forEach((pages) => {
+                let imagePage = pages.getAttribute("data-name");
+
+                if (imagePage == navName) {
+                    pages.classList.add("active");
+                } else {
+                    pages.classList.add("hide");
+                    pages.classList.remove("active");
+                }
+            })
+        } 
     }
-    myPorts[index].classList.add('active');
-    
 }
 
-const nextPage = (currentPort, targetPort) => {
-    currentPort.classList.remove('active');
-    targetPort.classList.add('active');
-    console.log("asdffasdg")
-}
-
-// const previousPage = (currentPort, targetPort) => {
-//     currentPort.classList.remove('active');
-//     targetPort.classList.add('active');
-//     console.log("asdf")
-// }
-
-// Click the nav move to next portfolio
-navPort.addEventListener('click', e => {
-    const targetNav = e.target.closest(".folio");
-    index = 0;
-
-    if (!targetNav) return;
-
-    const currentNav = navPort.querySelector(".active");
-    const currentPort = myPort.querySelector(".active");
-    const nextPort = currentPort.nextElementSibling;
-    // const previousPort = currentPort.previousElementSibling;
-
-    console.log(navPorts);
-
-    updateNav(currentNav, targetNav);
-    nextPage(currentPort, nextPort);
-    // previousPage(currentPort, previousPort);
-});
